45cbbe3307c058c2f89b1d23d94c1e11ca566fcd
Yut8853 <factory0611@gmail.com>
2024-03-13 09:24:51 +0900

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
     clean: true,
-    publicPath: '/toplace/color-ver2/', // この行を追加
+    publicPath: '/', // この行を追加
   },
   module: {
     rules: [
