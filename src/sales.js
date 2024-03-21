4f875c1ac805608e381dca39b0c2237389db5b9d
Yut8853 <factory0611@gmail.com>
2024-03-21 11:15:08 +0900

    font調整

--- src/sales.js
+++ src/sales.js
@@ -6,21 +6,5 @@ import './light2.js'
 import './hamburger.js';
 import './smooth.js'
 
-document.addEventListener('DOMContentLoaded', function () {
-    const images = document.querySelectorAll('.hover-image');
-
-    images.forEach(image => {
-        image.addEventListener('mouseover', function () {
-            document.body.style.backgroundImage = `url('${image.src}')`;
-            document.body.style.backgroundRepeat = 'repeat'; // 画像をタイル状に繰り返し表示
-        });
-
-        image.addEventListener('mouseout', function () {
-            document.body.style.backgroundImage = '';
-        });
-    });
-});
-
-
 
 console.log('sales.js')
