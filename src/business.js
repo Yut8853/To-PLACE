53c9151f92e2bd64c03a248d71b2f18bd53eb8dd
Yut8853 <factory0611@gmail.com>
2024-03-31 13:27:24 +0900

    scrooltrgger変更

--- src/business.js
+++ src/business.js
@@ -5,8 +5,30 @@ import './light.js'
 import './hamburger.js';
 import './smooth.js'
 import './under-start.js'
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger) 
+
+// 読み込み
+document.addEventListener('DOMContentLoaded', () => {
+  const textElements = gsap.utils.toArray('.text-effect');
+  
+  textElements.forEach(text => {
+    gsap.to(text, {
+      backgroundSize: '100%',
+      ease: 'power1.inOut',
+      scrollTrigger: {
+        trigger: text,
+        start: 'top bottom', // 要素の上端がビューポートの下端に達した時点で発火
+        end: 'top 70%',
+        scrub: true,
+        once: true,
+        // markers: true,
+      },
+    });
+  });
+});
 
 // ScrollTriggerをgsapに登録
 gsap.registerPlugin(ScrollTrigger);
--- src/policy.js
+++ src/policy.js
@@ -5,6 +5,30 @@ import './light.js'
 import './hamburger.js';
 import './smooth.js'
 import './under-start.js'
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger) 
+
+// 読み込み
+document.addEventListener('DOMContentLoaded', () => {
+  const textElements = gsap.utils.toArray('.text-effect');
+  
+  textElements.forEach(text => {
+    gsap.to(text, {
+      backgroundSize: '100%',
+      ease: 'power1.inOut',
+      scrollTrigger: {
+        trigger: text,
+        start: 'top bottom', // 要素の上端がビューポートの下端に達した時点で発火
+        end: 'top 70%',
+        scrub: true,
+        once: true,
+        // markers: true,
+      },
+    });
+  });
+});
 
 if (document.body.classList.contains('index-page')) {
     // index.htmlの場合にのみ実行するコード
