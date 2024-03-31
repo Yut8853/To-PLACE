131679d052a5050427de889b361345be1bfd7fe8
Yut8853 <factory0611@gmail.com>
2024-03-31 13:24:19 +0900

    テキストアニメーション追加

--- src/business.js
+++ src/business.js
@@ -5,6 +5,11 @@ import './light.js'
 import './hamburger.js';
 import './smooth.js'
 import './under-start.js'
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+// ScrollTriggerをgsapに登録
+gsap.registerPlugin(ScrollTrigger);
 
 // TOPページでのみ music.js を動的にインポート
 if (document.body.classList.contains('top-page')) {
--- src/scrollTrigger.js
+++ src/scrollTrigger.js
@@ -3,78 +3,82 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger) 
 
-gsap.fromTo('.parallax-right', {
-    x: "15.1797px",
-  }, {
-    x: '-3603.18px',
-    scrollTrigger: {
-      trigger: '.video-section', // Changed to the actual class of the scroll container
-      start: 'top 80%',
-      end: 'bottom top',
-      scrub: .1, // You can adjust the scrub speed if needed
-    }
-});
-
-// Move .parallax-left in the opposite direction, adjust values as needed
-gsap.fromTo('.parallax-left', {
-    x: "-1000.1797px", // Start moving from the opposite direction
-  }, {
-    x: '3603.18px', // Move to the opposite direction
-    scrollTrigger: {
-      trigger: '.video-section', // Ensuring consistency with the trigger element
-      start: 'top 80%',
-      end: 'bottom top',
-      scrub: 1, // Adjusting the scrub speed for a different effect
-    }
-});
-
-const textElements = gsap.utils.toArray('.text-effect');
-
-textElements.forEach(text => {
-  gsap.to(text, {
-    backgroundSize: '100%',
-    ease: 'power1.inOut',
-    scrollTrigger: {
-      trigger: text,
-      start: 'top bottom', // 要素の上端がビューポートの下端に達した時点で発火
-      end: 'top 70%',
-      scrub: true,
-      once: true,
-      markers: true,
-    },
+// 読み込み
+document.addEventListener('DOMContentLoaded', () => {
+  const textElements = gsap.utils.toArray('.text-effect');
+  
+  textElements.forEach(text => {
+    gsap.to(text, {
+      backgroundSize: '100%',
+      ease: 'power1.inOut',
+      scrollTrigger: {
+        trigger: text,
+        start: 'top bottom', // 要素の上端がビューポートの下端に達した時点で発火
+        end: 'top 70%',
+        scrub: true,
+        once: true,
+        // markers: true,
+      },
+    });
   });
-});
+  
+  gsap.fromTo('.parallax-right', {
+      x: "15.1797px",
+    }, {
+      x: '-3603.18px',
+      scrollTrigger: {
+        trigger: '.video-section', // Changed to the actual class of the scroll container
+        start: 'top 80%',
+        end: 'bottom top',
+        scrub: .1, // You can adjust the scrub speed if needed
+      }
+  });
 
-const word = document.querySelector('.link-effect');
-const letters = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
-let isIntervalRunning = false;
-let interval;
+  // Move .parallax-left in the opposite direction, adjust values as needed
+  gsap.fromTo('.parallax-left', {
+      x: "-1000.1797px", // Start moving from the opposite direction
+    }, {
+      x: '3603.18px', // Move to the opposite direction
+      scrollTrigger: {
+        trigger: '.video-section', // Ensuring consistency with the trigger element
+        start: 'top 80%',
+        end: 'bottom top',
+        scrub: 1, // Adjusting the scrub speed for a different effect
+      }
+  });
 
-// アニメーションを開始する関数
-const startAnimation = () => {
-  if (!isIntervalRunning) {
-    isIntervalRunning = true;
-    interval = setInterval(() => {
-      if (!isIntervalRunning) return;
-      const currentWord = word.innerHTML;
-      word.innerHTML = currentWord.split("").map(letter => letters[Math.floor(Math.random() * letters.length)]).join("");
-    }, 30);
 
-    // 3秒後にアニメーションを停止
-    setTimeout(() => {
-      clearInterval(interval);
-      isIntervalRunning = false;
-      word.innerHTML = 'GET IN TOUCH';
-    }, 1000);
-  }
-};
+  const word = document.querySelector('.link-effect');
+  const letters = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
+  let isIntervalRunning = false;
+  let interval;
 
-// GSAPのスクロールトリガーを使用して表示領域に入ったときにアニメーションを開始
-gsap.to(word, {
-  scrollTrigger: {
-    trigger: word,
-    start: "top bottom", // 要素がビューポートの下に入った時
-    onEnter: startAnimation,
-    once: true // 一度だけ実行
-  }
-});
+  // アニメーションを開始する関数
+  const startAnimation = () => {
+    if (!isIntervalRunning) {
+      isIntervalRunning = true;
+      interval = setInterval(() => {
+        if (!isIntervalRunning) return;
+        const currentWord = word.innerHTML;
+        word.innerHTML = currentWord.split("").map(letter => letters[Math.floor(Math.random() * letters.length)]).join("");
+      }, 30);
+
+      // 3秒後にアニメーションを停止
+      setTimeout(() => {
+        clearInterval(interval);
+        isIntervalRunning = false;
+        word.innerHTML = 'GET IN TOUCH';
+      }, 1000);
+    }
+  };
+
+  // GSAPのスクロールトリガーを使用して表示領域に入ったときにアニメーションを開始
+  gsap.to(word, {
+    scrollTrigger: {
+      trigger: word,
+      start: "top bottom", // 要素がビューポートの下に入った時
+      onEnter: startAnimation,
+      once: true // 一度だけ実行
+    }
+  });
+})
